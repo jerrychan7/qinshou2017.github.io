3b34e420c268283129122c6a2989e60b34b97c51
qinshou2017 <czy-ken@163.com>
2019-08-14 00:31:52 +0800

    now can change the address bar color at Chrome for Android based on theme

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function delCookie(cname) {
     setCookie(cname, "", -1);
 }
 
+var meta_theme_color = {};
 function retheme(theme) {
     var cn = document.body.className,
         allTheme = ("default,darktheme"/*allTheme*/).split(",");
@@ -37,8 +38,33 @@ function retheme(theme) {
     if (nextTheme != "default")
         document.body.className = (cn + " " + nextTheme).trim();
     setCookie("theme", nextTheme);
+    
+    var meta = document.querySelector("meta[name=\"theme-color\"]");
+    if (meta) meta.content = meta_theme_color[nextTheme];
 }
 
 window.addEventListener("load", function(e) {
+    getThemeColor(document.styleSheets);
+    function getThemeColor(sss) {
+        for (var i = 0; i < sss.length; ++i) {
+            var ss = sss[i], cr = ss.cssRules;
+            if (!cr) continue;
+            for (var j = 0; j < cr.length; ++j) {
+                var csr = cr[j];
+                if ("href" in csr) {
+                    getThemeColor([csr.styleSheet]);
+                    continue;
+                }
+                var st = csr.selectorText
+                if (st && st.indexOf("meta-theme-color") > -1) {
+                    var themeName = st.replace("meta-theme-color", "").trim();
+                    if ((".#").indexOf(themeName.charAt(0)) > -1)
+                        themeName = themeName.substring(1);
+                    meta_theme_color[themeName] = csr.style.color;
+                }
+            }
+        }
+    }
+
     retheme(getCookie("theme") || "default");
 });
