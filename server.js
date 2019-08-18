4447284753cd0503d7004c72d67d4463e02cc3ba
qinshou2017 <czy-ken@163.com>
2019-08-18 18:42:19 +0800

    a local static server is added to facilitate debugging

---
+++ server.js
@@ -0,0 +1,33 @@
+
+const http = require("http"),
+      fs = require("fs"),
+      url = require("url"),
+      path = require("path");
+let server = http.createServer(function(request, response) {
+    let pathObj = url.parse(request.url, true);
+    if (pathObj.pathname === "/" || pathObj === "/index")
+        pathObj.pathname = "/index.html";
+    let filePath = path.join(path.resolve(), pathObj.pathname);
+    fs.readFile(filePath, "binary", function(err, fileContent) {
+        if (err) {
+            console.log("404 " + filePath);
+            response.writeHead(404, "not found");
+            response.end("<h1>404 Not Found</h1>");
+        }
+        else {
+            console.log("ok " + filePath);
+            response.write(fileContent, "binary");
+            response.end();
+        }
+    });
+});
+server.listen(3000);
+console.log("visit http://localhost:3000 \nenter 'exit' to close the program.");
+let rl = require("readline").createInterface({input: process.stdin});
+rl.on("line", async line => {
+    if (line.trim() === "exit") {
+        server.close();
+        process.exitCode = 0;
+        rl.close();
+    }
+});
