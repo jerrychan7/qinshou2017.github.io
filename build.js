c5a0d20bb931c93fd66fbfce44a183ae10072266
qinshou2017 <czy-ken@163.com>
2024-09-09 18:50:14 +0800

    fix tags page has no content

--- build.js
+++ build.js
@@ -196,7 +196,7 @@ function refreshOtherFiles() {
 
   fs.writeFileSync(path.join(rootDirPath, "/tags.html"),
     fs.readFileSync(path.join(templateDirPath, "/tags.html"), "utf8")
-    .replace("{/*tags-title*/}", JSON.stringify(tags_title))
+    .replace(/\{\s*\/\*tags-title\*\/\s*\}/g, JSON.stringify(tags_title))
     .replace("{links}", links));
 
   fs.writeFileSync(path.join(rootDirPath, "/about.html"),
